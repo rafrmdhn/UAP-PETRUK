a08196f261bf75f937490a45db87566085bfd396
Alfa Rizki Fadlillah <92286829+alfarzf@users.noreply.github.com>
2022-06-14 23:28:18 +0700

    Update tutordek.cpp

--- tutordek.cpp
+++ tutordek.cpp
@@ -3,36 +3,50 @@
 using namespace std;
 
 int main();
-stack<int> piring;
+string nama;
+stack<int> tumpuk;
 int total=0, pilih, n;
 
 int pilihan(int pilihan){
     if(pilihan==1){
-        cout<<"PROGRAM TUMPUKAN C++"<<endl;
-        cout<<"Masukkan Jumlah Piring : ";
+        cout<<"Masukkan nama benda : ";
+        cin.ignore(512,'\n');
+        getline(cin,nama);
+        cout<<"Masukkan Jumlah "<<nama<<" : ";
         cin>>n;
-        piring.push(n);
+        tumpuk.push(n);
     }else if(pilihan==2){
-        if(piring.empty()){
+        if(tumpuk.empty()){
             cout<<"Tumpukan Kosong\n";
         }else{
-        cout<<"Jumlah Tumpukan Piring : ";
-        for(int i=0;i<=piring.size();i++){
-            total+=piring.top();
-            piring.pop();
+        cout<<"Jumlah Tumpukan "<<nama<<" : ";
+        for(int i=0;i<=tumpuk.size();i++){
+            total+=tumpuk.top();
+            tumpuk.pop();
          }cout<<total;
         }
     }
-    else{
+    else if(pilihan==3){
         return 0;
+    }
+    else{
+       cout<<"Pilihan anda tidak valid\n";
     }
     return main();
 }
 void menu(){
-    cout<<"\n1. Tambah Tumpukan\n2. Jumlah Tumpukan\n3. Keluar\nPilihan : ";
+    string menu[4]={"\nPROGRAM TUMPUKAN C++",
+                    "\n1. Tambah Tumpukan\n",
+                    "2. Jumlah Tumpukan\n",
+                    "3. Keluar\n"};
+    for(int i=0;i<4;i++){
+        cout<<menu[i];
+    }
 }
+
 int main(){
-    menu()
+    menu();
+    cout<<"Pilihan : ";
     cin>>pilih;
     pilihan(pilih);
 }
