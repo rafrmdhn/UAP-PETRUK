b7b160d553c796bcb9caf994f3227659fd95e69e
Muhammad Rafi Satria <89404089+Rapipipu@users.noreply.github.com>
2022-06-14 23:03:28 +0700

    Update tutordek.cpp

--- tutordek.cpp
+++ tutordek.cpp
@@ -8,7 +8,7 @@ int total=0, pilih, n;
 
 int pilihan(int pilihan){
     if(pilihan==1){
-        cout<<"PROGRAM TUMPUKAN PIRING"<<endl;
+        cout<<"PROGRAM TUMPUKAN C++"<<endl;
         cout<<"Masukkan Jumlah Piring : ";
         cin>>n;
         piring.push(n);
