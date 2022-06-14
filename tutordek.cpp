4538602e3e5e65097b38cf713470a1c288481c01
Alfa Rizki Fadlillah <92286829+alfarzf@users.noreply.github.com>
2022-06-14 22:56:44 +0700

    Update tutordek.cpp

--- tutordek.cpp
+++ tutordek.cpp
@@ -1,3 +1,35 @@
 #include <iostream>
+#include <stack>
 using namespace std;
-int main
+
+int main();
+stack<int> piring;
+int total=0, pilih, n;
+
+int pilihan(int pilihan){
+    if(pilihan==1){
+        cout<<"Masukkan Jumlah Piring : ";
+        cin>>n;
+        piring.push(n);
+    }else if(pilihan==2){
+        if(piring.empty()){
+            cout<<"Tumpukan Kosong\n";
+        }else{
+        cout<<"Jumlah Tumpukan Piring : ";
+        for(int i=0;i<=piring.size();i++){
+            total+=piring.top();
+            piring.pop();
+         }cout<<total;
+        }
+    }
+    else{
+        return 0;
+    }
+    return main();
+}
+
+int main(){
+    cout<<"\n1. Tambah Tumpukan\n2. Jumlah Tumpukan\n3. Keluar\nPilihan : ";
+    cin>>pilih;
+    pilihan(pilih);
+}
